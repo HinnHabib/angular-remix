8c06ab047536c90745e7efa8ed2531b2ebbc1811
Habib Hinn <habibissahinn@gmail.com>
2023-01-21 11:28:01 +0200

    Add Angular Pages

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SecondPageComponent } from './pages/second-page/second-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+  },
+  {
+    path: 'second-page',
+    component: SecondPageComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking'
-})],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,22 +1,28 @@
+import { SecondPageComponent } from './pages/second-page/second-page.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
-      HeaderComponent,
-      FooterComponent
-   ],
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    AboutUsComponent,
+    SecondPageComponent,
+  ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
